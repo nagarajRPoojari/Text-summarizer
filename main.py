d73607fc01accb37c519ca5690d7630c732c7de2
nagarajRPoojari <np137270@gmail.com>
2023-08-02 20:07:24 +0530

    data_validation_done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.logging import logger
 STAGE='stage_01 Data ingestion'
 
@@ -11,5 +12,14 @@ except Exception as e:
     print(f'---------  {STAGE} failed  ---------')
     logger.exception(e)
     
-
+STAGE='stage_01 Data validation'
     
+    
+try:
+    print(f'---------  {STAGE}  started ---------')
+    data_validation_pipeline = DataValidationTrainingPipeline()
+    data_validation_pipeline.main()
+    print(f'---------  {STAGE} completed  ---------')
+except Exception as e:
+    print(f'---------  {STAGE} failed  ---------')
+    logger.exception(e)
--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -1,9 +1,16 @@
 from dataclasses import dataclass
 from pathlib import Path
 
-@dataclass
+@dataclass(frozen=True)
 class DataIngetionConfig:
     root_dir:Path 
     source_url:str
     local_data_file:Path
     unzip_dir:Path
+    
+    
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir:Path
+    STATUS_FILE:str
+    ALL_REQUIRED_FILES:list
